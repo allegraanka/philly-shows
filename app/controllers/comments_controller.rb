bf8f0099e094dfe47c2a4a6b68dfe4ce5ea2adbd
Allegra Anka <allegra.anka@gmail.com>
2018-04-20 16:09:01 -0400

    comments almost done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,40 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+    puts params.inspect
+    write_comment
   end
 
   def index
+    @comment = Comment.all.order("created_at DESC")
   end
 
   def show
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
   end
 
   def update
+    edit_comment
   end
 
   def edit
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
   end
 
   def destroy
+    delete_comment
+    redirect_to user_post_path
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,27 @@
 module CommentsHelper
+
+  def write_comment
+    @comment = Comment.new(comment_params)
+    @comment.user_id = session[:user_id]
+    if @comment.save
+      redirect_to dashboard_path(User.find(session[:user_id]))
+    else
+      render 'index'
+    end
+  end
+
+  def edit_comment
+    @comment = Comment.find(params[:id])
+      if @comment.update(comment_params)
+        redirect_to dashboard_path(User.find(session[:user_id]))
+      else
+        render 'show'
+      end
+  end
+
+  def delete_comment
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
 end
